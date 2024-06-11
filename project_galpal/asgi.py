f4e2b1ff7651ee977b44893642caeed2624642ba
Caitlin Schaeffer <caitlinschaef@gmail.com>
2024-06-11 08:48:46 -0400

    found all errant names

--- project_galpal/asgi.py
+++ project_galpal/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_image_upload.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_galpal.settings')
 
 application = get_asgi_application()
--- project_galpal/wsgi.py
+++ project_galpal/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_image_upload.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_galpal.settings')
 
 application = get_wsgi_application()
